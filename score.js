a082f3278f170f911a8df5323ed664d0e9e45a8e
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-26 11:22:22 +0200

    Updates

---
+++ score.js
@@ -0,0 +1,50 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var reset = document.querySelector("#reset");
+var numInput = document.querySelector("input");
+var playingTo = document.querySelector("p");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
+
+p1Button.addEventListener("click", function () {
+    if (!gameOver)
+        p1Score++;
+    p1Display.textContent = p1Score;
+    if (p1Score === winningScore) {
+        p1Display.classList.add("winner");
+        gameOver = true;
+    }
+});
+p2Button.addEventListener("click", function () {
+    if (!gameOver)
+        p2Score++;
+    p2Display.textContent = p2Score;
+    if (p2Score === winningScore) {
+        p2Display.classList.add("winner");
+        gameOver = true;
+    }
+});
+reset.addEventListener("click", function () {
+    reseter();
+})
+
+function reseter() {
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+
+    gameOver = false;
+}
+
+numInput.addEventListener("change", function () {
+    reseter();
+    winningScore = parseInt(numInput.value);
+    playingTo.textContent = "Playing to: " + winningScore;
+})
